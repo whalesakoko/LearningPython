fde453f37bc058dacaec8f060f19edd581053650
whalesakoko <adesiyanmichael123@gmail.com>
2026-04-29 20:55:49 +0100

    commiting all my files into github

---
+++ main.py
@@ -0,0 +1,37 @@
+#name = input("Tell us your name: ")
+#age = input("Tell us your age: ")
+
+#print("Hello " + name + "!" "," + " your age is " + age)
+
+#STRING
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = num1 + num2
+#print(result)
+
+#INTEGER
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = int(num1) + int(num2)
+#print(result)
+
+#FLOAT
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = float(num1) + float(num2)
+#print(result)
+
+#MAD LIBS GAME
+
+#colour = input("Enter a colour: ")
+#plural_Noun = input("Enter a plural Noun: ")
+#celebrity = input("Enter a celebrity: ")
+#print("Roses are " + colour)
+#print(plural_Noun + " are blue")
+#print("I love you " + celebrity)
+
+name = "Mike"
+print(name)
+
+
+
